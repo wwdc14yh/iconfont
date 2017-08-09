291756162199186f3a4f562b78cec15b7c3eba9d
WWDC14 <wwdc14@vip.qq.com>
2017-08-09 14:38:27 +0800

    Update filter.rb

--- filter.rb
+++ filter.rb
@@ -1,85 +1,55 @@
-#! /bin/sh/ruby
 require 'pathname'
 require 'rexml/document'
 include REXML
 
-$outpath = File.dirname(__FILE__)
+class GeneraateFile
+    attr_accessor :source_file
+    attr_accessor :destination_header
+    attr_accessor :destination_implementation
 
-def paramCheck
-
-  helpString = <<-EOB
-  Usage: svg2oc <svgFile> [-out <outpath>]
-  EOB
-
-  if ARGV.count < 1 then
-    puts helpString
-    exit(1)
-  end
-
-  if ARGV.count != 1  && ARGV.count != 3  then
-    puts "Param Error!"
-    puts helpString
-    exit(1)
-  end
-
-  if !File.exist?(ARGV[0]) then
-    puts "No such file: " + ARGV[0]
-    exit(1)
-  end
-
-  if ARGV.count == 3 then
-    if ARGV[1].to_s == "-out" then
-      $outpath = ARGV[2]
-      if !File.exist?($outpath) then
-        puts "No such directory: " + $outpath
-        exit(1)
-      end
+    def initialize(source_file, destination_header, destination_implementation)
+        @source_file = source_file
+        @destination_header = destination_header
+        @destination_implementation = destination_implementation
     end
-  end
-end
 
-def parserXML
-  xml_file = ARGV[0]
-  elements = Hash.new
-  doc = Document.new(File.new(xml_file))
+    def generateHFile(iconfontMap)
+        header = author + '//  Created by AutoCoding on ' + Time.new.strftime('%Y/%m/%d') + ".\n" + headerPropertry
 
-  XPath.each(doc,'//glyph') {|e|
-    if e.attributes.count == 3
-      glyph_name = e.attributes['glyph-name']
-      glyph_symbol = jointParemeter(glyph_name)
-      glyph_code = e.attributes['unicode'].ord.to_s(16)
-      elements[glyph_symbol] = glyph_code
+        iconfontMap.keys.each do |key|
+            header = header + '@property (readonly) NSString *icon' + key.to_s + ";\n\n"
+        end
+        header += '@end'
+        header
     end
-  }
-  return elements
-end
 
-def jointParemeter(val)
-  if val.include? '-'
-    names = val.split('-')
-    jointString = ''
+    def generateMFile(iconFontName, iconfontMap)
+        implementation = author + '//  Created by AutoCoding on ' + Time.new.strftime('%Y/%m/%d') + ".\n" + implementationFile + '    UIFont *font = [UIFont fontWithName:@"' + iconFontName + '" size:size];' + "\n"
+        implementation += "
+          return font;
+      }\n \n"
 
-      names.each do |name|
-        jointString.concat(name.capitalize)
-      end
-      return jointString
-  end
+        iconfontMap.each do |key, value|
+            implementation = implementation + '- (NSString *)icon' + key.to_s + " {\n"
+            implementation = implementation + '    return @"\\U0000' + value + '";' + "\n}\n\n"
+        end
+        implementation += '@end'
+        implementation
+    end
 
-  return val.capitalize
-end
-
-def generateHFile(iconfontMap)
-  string1 = <<-EOB
+    def author
+        string = <<-EOF
 //
-//  IconFont.h
+//  YppIconFont.h
 //  AutoCoding
 //
-  EOB
+EOF
+    end
 
-  string2 = <<-EOB
+    def headerPropertry
+        string = <<-HEADER
 //  Copyright © 2017年 olinone. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
 @import CoreText;
 
@@ -90,29 +60,11 @@ def generateHFile(iconfontMap)
 + (instancetype)shareInstance;
 
 + (UIFont *)iconFontWithSize:(CGFloat)size;
-EOB
+HEADER
+    end
 
-  hString = string1 + "//  Created by AutoCoding on " + Time.new.strftime("%Y/%m/%d") + ".\n" + string2
-
-  iconfontMap.keys.each do |key|
-    hString = hString + "@property (readonly) NSString *icon" + key.to_s + ";\n\n"
-  end
-
-  hString = hString + "@end"
-
-  return hString
-end
-
-def generateMFile(iconFontName, iconfontMap)
-  iconFont = "iconfont"
-  string1 = <<-EOB
-//
-//  YppIconFont.m
-//  AutoCoding
-//
-  EOB
-
-  string2 = <<-EOB
+    def implementationFile
+        implementation = <<-EOB
 //  Copyright © 2016年 olinone. All rights reserved.
 //
 
@@ -129,52 +81,84 @@ def generateMFile(iconFontName, iconfontMap)
     return iconFont;
 }
 
-
-
 + (UIFont *)iconFontWithSize:(CGFloat)size {
 EOB
+    end
 
-  mString = string1 + "//  Created by AutoCoding on " + Time.new.strftime("%Y/%m/%d") + ".\n" + string2 + "    UIFont *font = [UIFont fontWithName:@\"" + iconFontName + "\" size:size];" + "\n"
-  mString = mString + "
-    return font;
-}\n \n"
-
-  iconfontMap.each { |key, value|
-    mString = mString + "- (NSString *)icon" + key.to_s + " {\n";
-    mString = mString + "    return @\"\\U0000" + value + "\";" + "\n}\n\n"
-  }
-
-  mString = mString + "@end"
-
-  return mString
+    def writeToFile(text, path)
+        hio = File.open(path, 'w+')
+        hio.puts(text)
+        hio.close
+    end
 end
 
+class Check
+    def helpString
+        'Usage: svg2oc <svgFile> [-out <outpath>]'
+    end
 
-def putStringToFile(text, path)
-  hio = File.open(path, "w+")
-  hio.puts(text)
-  hio.close
+    def safeCheck
+        if ARGV.count < 1 || ARGV.count != 1 && ARGV.count != 3
+            puts helpString
+            exit(1)
+        end
+
+        unless File.exist?(ARGV[0])
+            puts 'No such file: ' + ARGV[0]
+            exit(1)
+        end
+    end
+end
+
+class ParserSVG
+    attr_accessor :svg_file
+    def initialize(svg_file)
+        @svg_file = svg_file
+    end
+
+    def beginParese
+        doc = Document.new(File.new(@svg_file))
+        elements = {}
+        XPath.each(doc, '//glyph') do |e|
+            next unless e.attributes['horiz-adv-x'] == '1024'
+            glyph_name = e.attributes['glyph-name']
+            glyph_symbol = jointParemeter(glyph_name)
+            glyph_code = e.attributes['unicode'].ord.to_s(16)
+            elements[glyph_symbol] = glyph_code
+        end
+        elements
+    end
+
+    def jointParemeter(val)
+        if val.include? '-'
+            names = val.split('-')
+            jointString = ''
+            names.each do |name|
+                jointString.concat(name.capitalize)
+            end
+            return jointString
+        end
+        val.capitalize
+    end
 end
 
 # main
-paramCheck
+check = Check.new
+check.safeCheck
 
-inpath = ARGV[0]
-io = File.open(inpath)
-icon_font_map = parserXML
+parser = ParserSVG.new(ARGV[0])
+
+source_file = ARGV[0]
+io = File.open(source_file)
+icon_font_map = parser.beginParese
 io.close
 
-unless icon_font_map
-  p 'parse error'
-  exit(0)
-end
 # hfile
-hfilepath = $outpath+"/YppIconFont.h"
-putStringToFile(generateHFile(icon_font_map), hfilepath)
+hfilepath = File.dirname(__FILE__) + '/YppIconFont.h'
+mfilepath = File.dirname(__FILE__) + '/YppIconFont.m'
+generate = GeneraateFile.new(source_file, hfilepath, mfilepath)
+generate.writeToFile(generate.generateHFile(icon_font_map), generate.destination_header)
+generate.writeToFile(generate.generateMFile('iconfont', icon_font_map), generate.destination_implementation)
 
-# mfile
-iconfontName = 'iconfont'
-mfilepath = $outpath+"/YppIconFont.m"
-putStringToFile(generateMFile(iconfontName, icon_font_map), mfilepath)
+puts 'Done!'
 
-puts "Done!"
